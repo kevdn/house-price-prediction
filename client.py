44ed182cdb966190ca86c648b897287aa929bae7
kevdn <lehuukhoa12@gmail.com>
2024-05-29 23:00:46 +0700

    first attempt to deploy the model

--- client.py
+++ client.py
@@ -1,28 +1,44 @@
+import streamlit as st
 import requests
+server = "http://localhost:30001"
 
-headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
+st.title("House Price Prediction")
+
+# Input feature values
+MSSubClass = st.number_input("MSSubClass", min_value=20, max_value=190, value=60)
+MSZoning = st.text_input("MSZoning", "RL")
+LotArea = st.number_input("LotArea", min_value=1300, max_value=215000, value=7844)
+LotConfig = st.text_input("LotConfig", "Inside")
+BldgType = st.text_input("BldgType", "1Fam")
+OverallCond = st.number_input("OverallCond", min_value=1, max_value=10, value=7)
+YearBuilt = st.number_input("YearBuilt", min_value=1800, max_value=2020, value=1978)
+YearRemodAdd = st.number_input("YearRemodAdd", min_value=1800, max_value=2020, value=1978)
+Exterior1st = st.text_input("Exterior1st", "HdBoard")
+BsmtFinSF2 = st.number_input("BsmtFinSF2", min_value=0.0, max_value=2000.0, value=0.0)
+TotalBsmtSF = st.number_input("TotalBsmtSF", min_value=0.0, max_value=2000.0, value=672.0)
+
+# Create a dictionary with the data
+data = {
+    "MSSubClass" : MSSubClass,
+    "MSZoning" : MSZoning,
+    "LotArea" : LotArea,
+    "LotConfig" : LotConfig,
+    "BldgType" : BldgType,
+    "OverallCond" : OverallCond,
+    "YearBuilt" : YearBuilt,
+    "YearRemodAdd" : YearRemodAdd,
+    "Exterior1st" : Exterior1st,
+    "BsmtFinSF2" : BsmtFinSF2,
+    "TotalBsmtSF" : TotalBsmtSF
 }
 
-json_data = {
-    'MSSubClass': 60,
-    'MSZoning': 'RL',
-    'LotArea': 7844,
-    'LotConfig': 'Inside',
-    'BldgType': '1Fam',
-    'OverallCond': 7,
-    'YearBuilt': 1978,
-    'YearRemodAdd': 1978,
-    'Exterior1st': 'HdBoard',
-    'BsmtFinSF2': 0,
-    'TotalBsmtSF': 672,
-}
+# Make a request to the server
+response = requests.post(f"{server}/predict", json=data)
 
-# Post request to prediction endpoint.
-response = requests.post('http://localhost:30001/predict', headers=headers, json=json_data)
-if response.status_code == 200:
-    print("Successful!")
-    print(response.json())
-else:
-    print("Failed to get prediction!")
+# Get the result
+result = response.json()
+
+# Display the result
+st.write(f"The predicted price is: {result['price']}")
+
+
